24260c8e7f650e18d48329739859afebe3511f11
IGVDev <guidoverstraeten@gmail.com>
2023-03-20 11:13:58 -0300

    update template

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require("./config/app");
+const app = require("./src/config/app");
 
 const server = app.listen(process.env.port || 3001, () => {
   console.log(`Server listening on ${server.address().port}`);
--- src/config/db.js
+++ src/config/db.js
@@ -4,16 +4,18 @@ require("dotenv").config({ path: "config\\.env" });
 // Database connection
 const connectDb = async () => {
   try {
-    mongoose.connect(
-      process.env.NODE_DB_STRING,
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        tlsInsecure: true,
-      },
-      null
-    );
-    console.log("Connected");
+    if (process.env.NODE_DB_STRING !== undefined) {
+      mongoose.connect(
+        process.env.NODE_DB_STRING,
+        {
+          useUnifiedTopology: true,
+          useNewUrlParser: true,
+          tlsInsecure: true,
+        },
+        null
+      );
+      console.log("Connected");
+    }
   } catch (err) {
     console.log(err);
     process.exit(1);
